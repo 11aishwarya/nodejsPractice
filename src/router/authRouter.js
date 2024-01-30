cb81d757c1f69e55d17ea67dca1ad1e8817f66fc
Sanhil143 <sanhilrai143@gmail.com>
2024-01-31 00:16:32 +0530

    updated by sanhil

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { signup } = require('../controller/authController');
-const router = express.Router()
+const { signup, login } = require('../controller/authController');
+const router = express.Router();
 
 
-// router.get('/checkApi',(req,res) =>{
-//     res.send("i'm testing this api");
-// })
+router.get('/checkApi',(req,res) =>{
+    res.send("i'm testing this api");
+})
 
-router.post('/signup' , signup);
+router.post('/signup' ,signup);
+router.post('/login' ,login);
+
 
 module.exports = router;
